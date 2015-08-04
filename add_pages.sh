cef08e0d6ef5e12cfd47eb3e04e75a8bbcb92462
theMikeD <info@codenamemiked.com>
2015-08-04 09:49:57 -0400

    Minor updates

--- add_pages.sh
+++ add_pages.sh
@@ -8,8 +8,8 @@ wp post create --post_type=page --post_title="About" --post_status="publish"
 wp post create /Users/mike/git/Wordpress.tools/wp-base-tools/page-font-test.txt --post_type=page --post_title="Font Testing" --post_status="publish"
 
 # Taxonomy archive pages
-wp post create --post_type=page --post_title="Categories" --post_status="publish"
-wp post create --post_type=page --post_title="Tags" --post_status="publish"
+wp post create --post_type=page --post_title="Categories" --slug="category" --post_status="publish"
+wp post create --post_type=page --post_title="Tags" --slug="tag" --post_status="publish"
 wp post create --post_type=page --post_title="Venues" --post_status="publish" --post_name='wedding-venues'
 wp post create --post_type=page --post_title="Vendors" --post_status="publish" --post_name='wedding-vendors'
 wp post create --post_type=page --post_title="Photographers" --post_status="publish" --post_name='wedding-photographers'
@@ -26,11 +26,22 @@ wp plugin activate wp-sync-db
 
 
 # Inject top-level vendor terms
-wp term create vendortax 'Cakemakers and Bakers'
-wp term create vendortax 'Caterers'
+wp term create vendortax 'Cakemaker'
+wp term create vendortax 'Caterer'
 wp term create vendortax 'Couture'
-wp term create vendortax 'Florists and Event Designers'
-wp term create vendortax 'Hair and Makeup Artists'
-wp term create vendortax 'Musicians and DJs'
-wp term create vendortax 'Event Planners'
+wp term create vendortax 'Florist'
+wp term create vendortax 'Event Designer'
+wp term create vendortax 'Hair Designer'
+wp term create vendortax 'Makeup Artists'
+wp term create vendortax 'Live Music'
+wp term create vendortax 'DJs'
+wp term create vendortax 'Event Planner'
 
+
+wp term create category 'Portraits'
+wp term create category 'Engagements'
+wp term create category 'Studio News'
+
+
+# set time zone
+# rename uncategorized cat to Weddings
