7931d1e15bfbccaa106308980fa0a78cc91218b8
theMikeD <mike@codenamemiked.com>
2025-03-02 16:19:52 -0500

    re-arrange

--- wp-config.php
+++ wp-config.php
@@ -1,76 +1,75 @@
 <?php
 
-$cnmd_http = ($_SERVER['HTTPS']) ? 'https://': 'http://';
-$cnmd_server_name = rtrim($_SERVER['HTTP_HOST'],"/");
-$cnmd_document_root = rtrim(dirname(__FILE__),"/");
-
-
-// The wordpress install is in its own folder, so we force that setting here
-define('WP_SITEURL', $cnmd_http . $cnmd_server_name . '/wordpress');
-define('WP_HOME',    $cnmd_http . $cnmd_server_name);
-
-// The wordpress wp-content folder is outside of the git WordPress submodule so
-//     tell WP where it is. These may have to be set manually in some cases
-define('WP_CONTENT_URL', $cnmd_http . $cnmd_server_name . '/wp-content');
-define('WP_CONTENT_DIR', $cnmd_document_root . '/wp-content');
-
 // Four stages
 // local = local development on my workstation
 // staging = site is on my staging server
 // live = site is live and under my ongoing maintenance
-// released = site is live and I play no role in maintenance. In this case, the logic 
+// released = site is live and I play no role in maintenance. In this case, the logic
 //  block below is removed and everything reverts back to a single file, and all traces
 //  of git are purged leaving a normal site. This is all done manually btw.
-
 if ( file_exists( dirname( __FILE__ ) . '/wp-config-local.php' ) ) {
-  	define( 'CNMD_DEV_STATUS', 'local' );
-  	include( dirname( __FILE__ ) . '/wp-config-local.php' );
-	
+	define( 'CNMD_DEV_STATUS', 'local' );
+	include dirname( __FILE__ ) . '/wp-config-local.php';
+
 	// I don't need cron running, so turn it off
 	define( 'DISABLE_WP_CRON', true );
 
 	// Enable all debugging
-  	define('WP_DEBUG', false);
+	define( 'WP_DEBUG', false );
 
 	// Disable all core updates, as they are handled on local machine and done via git
- 	define( 'WP_AUTO_UPDATE_CORE', false );
-	define( 'DISALLOW_FILE_MODS', true );
+	define( 'WP_AUTO_UPDATE_CORE', false );
+	define( 'DISALLOW_FILE_MODS', false );
 	define( 'AUTOMATIC_UPDATER_DISABLED', true );
 
-} else if ( file_exists( dirname( __FILE__ ) . '/wp-config-staging.php' ) ) {
-  	define( 'CNMD_DEV_STATUS', 'staging' );
-	include( dirname( __FILE__ ) . '/wp-config-staging.php' );
+} elseif ( file_exists( dirname( __FILE__ ) . '/wp-config-staging.php' ) ) {
+	define( 'CNMD_DEV_STATUS', 'staging' );
+	include dirname( __FILE__ ) . '/wp-config-staging.php';
 
 	// I Handle cron via system cron, so disable wp_cron
 	define( 'DISABLE_WP_CRON', true );
 
 	// Disable all core updates, as they are handled on local machine and done via git
- 	define( 'WP_AUTO_UPDATE_CORE', false );
+	define( 'WP_AUTO_UPDATE_CORE', false );
 	define( 'DISALLOW_FILE_MODS', true );
 	define( 'AUTOMATIC_UPDATER_DISABLED', true );
 
-} else if ( file_exists( dirname( __FILE__ ) . '/wp-config-live.php' ) ) {
-  	define( 'CNMD_DEV_STATUS', 'live' );
-  	include( dirname( __FILE__ ) . '/wp-config-live.php' );
+} elseif ( file_exists( dirname( __FILE__ ) . '/wp-config-live.php' ) ) {
+	define( 'CNMD_DEV_STATUS', 'live' );
+	include dirname( __FILE__ ) . '/wp-config-live.php';
 
 	//Disable all core updates, as they are handled on local machine and done via git
- 	define( 'WP_AUTO_UPDATE_CORE', false );
+	define( 'WP_AUTO_UPDATE_CORE', false );
 	define( 'DISALLOW_FILE_MODS', true );
 	define( 'AUTOMATIC_UPDATER_DISABLED', true );
 
 	// Disable all debugging
-  	define('WP_DEBUG', false);
+	define( 'WP_DEBUG', false );
 }
 
 
-// NOTE: If upgrading from an old installation and DB_CHARSET and DB_COLLATE do not exist 
+$cnmd_http          = ( $_SERVER['HTTPS'] ) ? 'https://' : 'http://';
+$cnmd_server_name   = rtrim( $_SERVER['HTTP_HOST'], '/' );
+$cnmd_document_root = rtrim( dirname( __FILE__ ), '/' );
+
+
+// The WordPress install is in its own folder, so we force that setting here
+define( 'WP_SITEURL', $cnmd_http . $cnmd_server_name . '/wordpress' );
+define( 'WP_HOME', $cnmd_http . $cnmd_server_name );
+
+// The WordPress wp-content folder is outside of the git WordPress submodule so
+//     tell WP where it is. These may have to be set manually in some cases
+define( 'WP_CONTENT_URL', $cnmd_http . $cnmd_server_name . '/wp-content' );
+define( 'WP_CONTENT_DIR', $cnmd_document_root . '/wp-content' );
+
+// NOTE: If upgrading from an old installation and DB_CHARSET and DB_COLLATE do not exist
 //  in the wp-config.php file, DO NOT ADD THEM. Read more here
 //  https://codex.wordpress.org/Editing_wp-config.php#Database_character_set
 // Database Charset to use in creating database tables.
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
 
 // The Database Collate type. Don't change this if in doubt.
-define('DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
 
 /**
  * Authentication Unique Keys and Salts.
@@ -93,13 +92,14 @@ define('DB_COLLATE', '');
  * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
  * language support.
  */
-define('WPLANG', '');
+define( 'WPLANG', '' );
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once ABSPATH . 'wp-settings.php';
